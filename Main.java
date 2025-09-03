49d0e9ea1c0e870ec995706bc91cd0d359ffa741
Pin25063 <pin25063@uvg.edu.gt>
2025-09-02 19:00:47 -0600

    Este es el cuarto commit, al cerrar el archivo que se abre en VScode al parecer Este mensaje se coloca

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main{
         System.out.println("Hola Mundo");
         System.out.print("Segundo commit");
         System.out.println("Ayuda, me esta costando mucho");
+        System.out.println("Ok, creo que ya entendi");
     }
 }
